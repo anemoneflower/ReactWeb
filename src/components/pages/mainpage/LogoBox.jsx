1e8c2233a92493ba3b8b1fceabb81b5bef917bd3
seoyun <seoyun.son@kaist.ac.kr>
2020-01-21 21:31:48 +0900

    FIX: conver function to class

--- src/components/pages/mainpage/LogoBox.jsx
+++ src/components/pages/mainpage/LogoBox.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function LogoBox(props) {
+class LogoBox extends React.Component {
     /**
      * input props:
      *      src - image source
@@ -9,15 +9,17 @@ function LogoBox(props) {
      * 1. import LogoBox from './components/pages/mainpage/LogoBox';
      * 2. <LogoBox src='./../../../logo_simple.png' alt=""/>
      */
-    let{src, alt} = props
-    return (
-        <div style={{textAlign:"center"}}>
-            <img
-                src = {src}
-                alt = {alt}
-            />
-        </div>
-    );
+    render() {
+        let{src, alt} = this.props;
+        return (
+            <div style={{textAlign:"center"}}>
+                <img
+                    src = {src}
+                    alt = {alt}
+                />
+            </div>
+        );
+    }
 }
 
 export default LogoBox;
--- src/components/pages/mainpage/MainPage.jsx
+++ src/components/pages/mainpage/MainPage.jsx
@@ -7,29 +7,31 @@ import {Button} from 'react-bootstrap';
 
 
 
-function MainPage() {
-    return (
-        <Router>
-            <div>
-                <React.Fragment>
-                    {/* verification box template*/}
-                    <div align="right">
-                        <Button className="m-1" variant="outline-primary">Sign In</Button>
-                        <Button className="m-1" variant="outline-primary">Sign Up</Button>
-                    </div>
-                    {/* verification box template end */}
+class MainPage extends React.Component {
+    render(){
+        return (
+            <Router>
+                <div>
+                    <React.Fragment>
+                        {/* verification box template*/}
+                        <div align="right">
+                            <Button className="m-1" variant="outline-primary">Sign In</Button>
+                            <Button className="m-1" variant="outline-primary">Sign Up</Button>
+                        </div>
+                        {/* verification box template end */}
 
-                    <LogoBox src='./../../../logo_simple.png' alt=""/>
+                        <LogoBox src='./../../../logo_simple.png' alt=""/>
 
-                    {/* about box template */}
-                    <div align="center">
-                        <Button className="m-1">About</Button>
-                    </div>
-                    {/* about box template end */}
-                </React.Fragment>
-            </div>
-        </Router>
-    )
+                        {/* about box template */}
+                        <div align="center">
+                            <Button className="m-1">About</Button>
+                        </div>
+                        {/* about box template end */}
+                    </React.Fragment>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default MainPage;
